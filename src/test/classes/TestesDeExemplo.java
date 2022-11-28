70b4b8736e749bf5d46f8d9f363ca62b7687c77c
DevilsHand <gabriel88santos@gmail.com>
2022-11-28 12:35:39 -0300

    adicionando configuração do Logger e enriquecendo exemplos

--- src/test/classes/TestesDeExemplo.java
+++ src/test/classes/TestesDeExemplo.java
@@ -1,19 +1,27 @@
-package classes;
+package test.classes;
 
-import core.Hooks;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import test.SimulatedWait;
+import test.core.Hooks;
+import test.core.logger.LogWritter;
 
 public class TestesDeExemplo extends Hooks {
 
+
     @Test(testName = "Teste Exemplo 1")
     public void testeDeExemplo(){
+        LogWritter.logFieldValidation("Campo de teste 1");
         Assert.assertEquals("isso é verdade", "isso é verdade");
+        SimulatedWait.waitSecs(1);
+        LogWritter.logFieldValidation("Campo de teste 2", "isso também é verdade");
         Assert.assertEquals("isso também é verdade", "isso também é verdade");
+        SimulatedWait.waitSecs(2);
     }
 
     @Test(testName = "Teste Exemplo 2")
     public void testeDeExemplo2(){
-        Assert.assertEquals("isso é verdade", "isso nao é verdade");
+        LogWritter.logFieldValidation("Campo 3", "isso não é verdade");
+        Assert.assertEquals("isso nao eh verdade", "isso não é verdade");
     }
 }
